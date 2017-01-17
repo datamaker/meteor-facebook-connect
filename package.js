e760aab7049789dde475c636024bf93c91690767
권정빈 <datamaker@urbanbase.com>
2017-01-17 16:31:37 +0900

    권정빈 / 0.1.4 버전 업그레이드 , 회원가입  -> 메일인증 x - > SNS 통합시 문제점

--- package.js
+++ package.js
@@ -1,15 +1,19 @@
 Package.describe({
-  summary: "Facebook Connect package to connect facebook users to existing users."
+    name: "datamaker:facebook-connect",
+    version: '0.1.4',
+    summary: "Facebook Connect package to connect facebook users to existing users.",
+    git: 'https://github.com/datamaker/meteor-fb-connect',
+    documentation: 'README.md'
 });
 
 Package.on_use(function (api) {
-  api.use(['accounts-facebook'], ['client', 'server']);
+    api.use(['accounts-facebook'], ['client', 'server']);
 
-  //add dependency for overriding core
-  api.use('oauth-encryption', 'server', {weak: true});
-  api.use('oauth');
-  api.use('facebook', ['client', 'server']);
+    //add dependency for overriding core
+    api.use('oauth-encryption', 'server', {weak: true});
+    api.use('oauth');
+    api.use('facebook', ['client', 'server']);
 
-  api.add_files(["client.js"], 'client');
-  api.add_files(["server.js"], 'server');
+    api.add_files(["client.js"], 'client');
+    api.add_files(["server.js"], 'server');
 });
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var OAuthEncryption = Package["oauth-encryption"] && Package["oauth-encryption"]
 
 
 var usingOAuthEncryption = function () {
-  return OAuthEncryption && OAuthEncryption.keyIsLoaded();
+    return OAuthEncryption && OAuthEncryption.keyIsLoaded();
 };
 
 
@@ -22,12 +22,12 @@ var usingOAuthEncryption = function () {
 // the users collection.
 //
 var pinEncryptedFieldsToUser = function (serviceData, userId) {
-  _.each(_.keys(serviceData), function (key) {
-    var value = serviceData[key];
-    if (OAuthEncryption && OAuthEncryption.isSealed(value))
-      value = OAuthEncryption.seal(OAuthEncryption.open(value), userId);
-    serviceData[key] = value;
-  });
+    _.each(_.keys(serviceData), function (key) {
+        var value = serviceData[key];
+        if (OAuthEncryption && OAuthEncryption.isSealed(value))
+            value = OAuthEncryption.seal(OAuthEncryption.open(value), userId);
+        serviceData[key] = value;
+    });
 };
 
 ///
@@ -45,140 +45,140 @@ var pinEncryptedFieldsToUser = function (serviceData, userId) {
 // @returns {Object} Object with token and id keys, like the result
 //        of the "login" method.
 //
-Accounts.updateOrCreateUserFromExternalService = function(
-  serviceName, serviceData, options) {
-  options = _.clone(options || {});
+Accounts.updateOrCreateUserFromExternalService = function (serviceName, serviceData, options) {
+    options = _.clone(options || {});
 
-  if (serviceName === "password" || serviceName === "resume")
-    throw new Error(
-      "Can't use updateOrCreateUserFromExternalService with internal service "
-        + serviceName);
-  if (!_.has(serviceData, 'id'))
-    throw new Error(
-      "Service data for service " + serviceName + " must include id");
+    if (serviceName === "password" || serviceName === "resume")
+        throw new Error(
+            "Can't use updateOrCreateUserFromExternalService with internal service "
+            + serviceName);
+    if (!_.has(serviceData, 'id'))
+        throw new Error(
+            "Service data for service " + serviceName + " must include id");
 
-  var selector = Accounts.externalServiceSelector(serviceName, serviceData, options);
+    var selector = Accounts.externalServiceSelector(serviceName, serviceData, options);
 
-  if (! selector)
-    return false;
+    if (!selector)
+        return false;
 
-  var user = Meteor.users.findOne(selector);
+    var user = Meteor.users.findOne(selector);
 
-  if (user) {
-    pinEncryptedFieldsToUser(serviceData, user._id);
+    if (user) {
+        pinEncryptedFieldsToUser(serviceData, user._id);
 
-    // We *don't* process options (eg, profile) for update, but we do replace
-    // the serviceData (eg, so that we keep an unexpired access token and
-    // don't cache old email addresses in serviceData.email).
-    // XXX provide an onUpdateUser hook which would let apps update
-    //     the profile too
-    var setAttrs = {};
-    _.each(serviceData, function(value, key) {
-      setAttrs["services." + serviceName + "." + key] = value;
-    });
+        // We *don't* process options (eg, profile) for update, but we do replace
+        // the serviceData (eg, so that we keep an unexpired access token and
+        // don't cache old email addresses in serviceData.email).
+        // XXX provide an onUpdateUser hook which would let apps update
+        //     the profile too
+        var setAttrs = {};
+        _.each(serviceData, function (value, key) {
+            setAttrs["services." + serviceName + "." + key] = value;
+            setAttrs['emails.0.verified'] = true
+        });
 
-    // XXX Maybe we should re-use the selector above and notice if the update
-    //     touches nothing?
-    Meteor.users.update(user._id, {$set: setAttrs});
-    return {
-      type: serviceName,
-      userId: user._id
-    };
-  } else {
-    // Create a new user with the service data. Pass other options through to
-    // insertUserDoc.
-    user = {services: {}};
-    user.services[serviceName] = serviceData;
-    return {
-      type: serviceName,
-      userId: Accounts.insertUserDoc(options, user)
-    };
-  }
+        // XXX Maybe we should re-use the selector above and notice if the update
+        //     touches nothing?
+        Meteor.users.update(user._id, {$set: setAttrs});
+        return {
+            type: serviceName,
+            userId: user._id
+        };
+    } else {
+        // Create a new user with the service data. Pass other options through to
+        // insertUserDoc.
+        user = {services: {}};
+        user.services[serviceName] = serviceData;
+        return {
+            type: serviceName,
+            userId: Accounts.insertUserDoc(options, user)
+        };
+    }
 };
 
-Accounts.externalServiceSelector = function(
-  serviceName, serviceData, options){
-  var selector = false;
+Accounts.externalServiceSelector = function (serviceName, serviceData, options) {
+    var selector = false;
 
-  //check if specific selector is available for service
-  //eg externalServiceSelectorTwitter
-  var selectorMethod = "externalServiceSelector";
+    //check if specific selector is available for service
+    //eg externalServiceSelectorTwitter
+    var selectorMethod = "externalServiceSelector";
     selectorMethod += serviceName.charAt(0).toUpperCase() + serviceName.slice(1);
 
-  if(Accounts[selectorMethod]){
-    selector = Accounts[selectorMethod](serviceName, serviceData, options);
-  }
+    if (Accounts[selectorMethod]) {
+        selector = Accounts[selectorMethod](serviceName, serviceData, options);
+    }
 
-  // Look for a user with the appropriate service user id.
-  if(!selector && !! serviceData.id) {
-    var serviceIdKey = "services." + serviceName + ".id";
-    selector[serviceIdKey] = serviceData.id;
-  }
+    // Look for a user with the appropriate service user id.
+    if (!selector && !!serviceData.id) {
+        var serviceIdKey = "services." + serviceName + ".id";
+        selector[serviceIdKey] = serviceData.id;
+    }
 
-  return selector;
+    return selector;
 };
 
-Accounts.externalServiceSelectorTwitter = function(
-  serviceName, serviceData, options){
-  var selector = {};
-  // XXX Temporary special case for Twitter. (Issue #629)
-  //   The serviceData.id will be a string representation of an integer.
-  //   We want it to match either a stored string or int representation.
-  //   This is to cater to earlier versions of Meteor storing twitter
-  //   user IDs in number form, and recent versions storing them as strings.
-  //   This can be removed once migration technology is in place, and twitter
-  //   users stored with integer IDs have been migrated to string IDs.
-  if (!isNaN(serviceData.id)) {
-    selector["$or"] = [{},{}];
-    selector["$or"][0][serviceIdKey] = serviceData.id;
-    selector["$or"][1][serviceIdKey] = parseInt(serviceData.id, 10);
-  } else {
-    selector = false;
-  }
-  return selector;
+Accounts.externalServiceSelectorTwitter = function (serviceName, serviceData, options) {
+    var selector = {};
+    // XXX Temporary special case for Twitter. (Issue #629)
+    //   The serviceData.id will be a string representation of an integer.
+    //   We want it to match either a stored string or int representation.
+    //   This is to cater to earlier versions of Meteor storing twitter
+    //   user IDs in number form, and recent versions storing them as strings.
+    //   This can be removed once migration technology is in place, and twitter
+    //   users stored with integer IDs have been migrated to string IDs.
+    if (!isNaN(serviceData.id)) {
+        selector["$or"] = [{}, {}];
+        selector["$or"][0][serviceIdKey] = serviceData.id;
+        selector["$or"][1][serviceIdKey] = parseInt(serviceData.id, 10);
+    } else {
+        selector = false;
+    }
+    return selector;
 };
-
-
 
 
 /// this must remain in this package after pull request merge
 
 //our custom facebook selector to also select users on facebook-email
-Accounts.externalServiceSelectorFacebook = function(
-  serviceName, serviceData, options){
-  var serviceIdKey = "services." + serviceName + ".id";
-  var selector = {};
-  selector["$or"] = [{},{}];
-  selector["$or"][0][serviceIdKey] = serviceData.id;
-  //also check on email
-  selector["$or"][1]["emails.address"] = serviceData.email;
-  if (! serviceData.email)
-    selector = false;
-  return selector;
+Accounts.externalServiceSelectorFacebook = function (serviceName, serviceData, options) {
+    var serviceIdKey = "services." + serviceName + ".id";
+    var selector = {};
+    selector["$or"] = [{}, {}];
+    selector["$or"][0][serviceIdKey] = serviceData.id;
+    //also check on email
+    selector["$or"][1]["emails.address"] = serviceData.email;
+    if (!serviceData.email)
+        selector = false;
+    return selector;
 };
 
 Meteor.methods({
-  connectUserWithFacebook: function (token, secret) {
-    //errors
-    if (! this.userId)
-      throw new Meteor.Error(403, "user must be loggedin");
+    connectUserWithFacebook: function (token, secret) {
+        //errors
+        if (!this.userId)
+            throw new Meteor.Error(403, "user must be loggedin");
 
-    var user = Meteor.user();
-    if (user.services && user.services.facebook)
-      throw new Meteor.Error(403, "user can not have a facebook connected account");
+        var user = Meteor.user();
+        if (user.services && user.services.facebook)
+            throw new Meteor.Error(403, "user can not have a facebook connected account");
 
-    if (Meteor.isServer) {
-      var fbData = Facebook.retrieveCredential(token, secret);
+        if (Meteor.isServer) {
+            var fbData = Facebook.retrieveCredential(token, secret);
 
-      if(!fbData)
-        throw new Meteor.Error(403, "not able to retreive fb data");
+            if (!fbData)
+                throw new Meteor.Error(403, "not able to retreive fb data");
 
-      //check if no accounts exists for this facebook user
-      var existing = Meteor.users.find({'services.facebook.id': fbData.serviceData.id}).count();
-      if(existing)
-        throw new Meteor.Error(403, "user can not have a facebook connected account");
-
-      Meteor.users.update(this.userId, {$set: {'services.facebook': fbData.serviceData}});
+            //check if no accounts exists for this facebook user
+            var existing = Meteor.users.find({'services.facebook.id': fbData.serviceData.id}).count();
+            if (existing)
+                throw new Meteor.Error(403, "user can not have a facebook connected account");
+            // 페이스북 인증시 이메일 인증 true으로 변경
+            Meteor.users.update(this.userId, {
+                $set: {
+                    'services.facebook': fbData.serviceData,
+                    'emails.0.verified': true
+                }
+            });
+        }
     }
-  }
 });
