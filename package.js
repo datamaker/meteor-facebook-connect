60fa55917970bfc4def9475268e9008fd05264b9
권정빈 <datamaker@urbanbase.com>
2017-02-27 17:50:05 +0900

    권정빈 / 1.4.3.1 버전에 따라 패키지 업그레이드

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
     name: "datamaker:facebook-connect",
-    version: '0.1.4',
+    version: '0.1.5',
     summary: "Facebook Connect package to connect facebook users to existing users.",
     git: 'https://github.com/datamaker/meteor-fb-connect',
     documentation: 'README.md'
 });
 
 Package.on_use(function (api) {
-    api.use(['accounts-facebook@1.0.11'], ['client', 'server']);
+    api.use(['accounts-facebook@1.1.0'], ['client', 'server']);
 
     //add dependency for overriding core
     api.use('oauth-encryption@1.2.0', 'server', {weak: true});
-    api.use('oauth@1.1.11');
+    api.use('oauth@1.1.13');
     api.use('facebook@1.2.10', ['client', 'server']);
 
     api.add_files(["client.js"], 'client');
